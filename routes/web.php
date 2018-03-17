a6b0c9340a72bba06af72a4f1a360ac1525611c5
jose219 <jose219@gmail.com>
2018-03-17 12:41:18 +0100

    Base project

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+Route::get('/', 'TestController@welcome');
+   
+
+Route::get('/prueba', function() {
+	return 'Hola, soy la ruta prueba.';
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
